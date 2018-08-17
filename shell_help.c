7891161fe86e4109e03e19bd8144e5b1f238ce64
Maksym Komar <komar@evologics.de>
2018-08-17 09:43:15 +0200

    add forgeted shell_help.[ch] and TODO

---
+++ shell_help.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>    /* strcmp() */
+
+#include <shell.h>
+#include <shell_help.h>
+
+void shell_help_show_drivers(struct shell_config *sc, char *shift)
+{
+    struct driver_t *drv;
+
+    printf("%s<driver> is:\n", shift);
+    for (drv = sc->drivers; drv->name != NULL; drv++)
+         printf("\t%s%s.\n\t%s%s\n\n", shift, drv->usage, shift, drv->help);
+}
+
+void shell_help_show(struct shell_config *sc, char *name)
+{
+    struct commands_t *cmd;
+    for (cmd = sc->commands; cmd->name != NULL; cmd++) {
+        if (!name || !strcmp(cmd->name, name)) {
+            printf ("%-10s-\t%s\n", cmd->name, cmd->help);
+            printf ("%-10s \tUsage: %s\n", " ", cmd->usage ? cmd->usage : cmd->name);
+            if (name) {
+                if (cmd->flags & SCF_USE_DRIVER)
+                    shell_help_show_drivers(sc, "\t\t");
+                break;
+            }
+        }
+    }
+
+    if (!name)
+        shell_help_show_drivers(sc, "");
+
+    if (name && cmd->name == NULL) {
+        fprintf(stderr, "Unknown topic: %s\n", name);
+    }
+}
+
---
+++ shell_help.h
@@ -0,0 +1,8 @@
+#ifndef SHELL_HELP_H
+#define SHELL_HELP_H
+
+void shell_help_show_drivers(struct shell_config *sc, char *shift);
+void shell_help_show(struct shell_config *sc, char *name);
+
+#endif
+
