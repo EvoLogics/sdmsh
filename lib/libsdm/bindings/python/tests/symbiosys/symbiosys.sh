53fc35be0e67a30c91844202aef325666cf84928
Maksym Komar <komar@evologics.de>
2020-05-05 15:27:44 +0200

    symbiosys.sh now not runned in gdb

--- lib/libsdm/bindings/python/tests/symbiosys/symbiosys.sh
+++ lib/libsdm/bindings/python/tests/symbiosys/symbiosys.sh
@@ -2,6 +2,7 @@
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-../../../..:../../../../../libstream}
 export PYTHONPATH=${PYTHONPATH:-../..}
 
-# [Inferior 1 (process 85751) exited normally
-gdb -return-child-result -ex run -ex "thread apply all bt" -ex quit --args python symbiosys.py $@
-#./symbiosys.py $@
+# Uncoment this for debug in gdb
+#gdb -return-child-result -ex run -ex "thread apply all bt" -ex quit --args python symbiosys.py $@
+
+./symbiosys.py $@
