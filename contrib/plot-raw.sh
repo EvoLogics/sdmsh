91fcb5a61fca3dc754dc9d1fad83fa11ca2308be
Maksym Komar <komar@evologics.de>
2021-05-09 07:13:39 +0200

    Fix plot-raw.sh to be interactive with gnuplot-qt

--- contrib/plot-raw.sh
+++ contrib/plot-raw.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 if [ -z "$1" ]; then
-    echo $0 file-s16.raw >&2
+    echo "$0 file-s16.raw [file-s16.raw]">&2
     exit 1
 fi
 
@@ -13,13 +13,15 @@ fi
 # set terminal svg;
 # set output 'test.png';
 
-if false; then
+if [ -n "$2" ]; then
     gnuplot -p -e "set multiplot layout 3,1 ; \
+    set grid xtics lt 0 lw 1 lc rgb '#bbbbbb'; \
     plot 0 ls 2, '$1' binary format='%int16' using 0:1 with lines ls 1; \
     plot 0 ls 2, '$2' binary format='%int16' using 0:2 with lines ls 1; \
-    unset multiplot"
+    unset multiplot\
+    pause mouse close"
     exit
 fi
 
-gnuplot -p -e "plot '$1' binary format='%int16' using 0:1 with lines;"
+gnuplot -e "set grid; plot '$1' binary format='%int16' using 0:1 with lines;pause mouse close"
 
